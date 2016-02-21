b6cba0dad4abf5650b2767afe38f1bfd059f1ed3
Evan Lay <evanrlay@gmail.com>
2016-02-21 21:44:49 +0000

    Made function to toggle more info view

--- JS/scripting.js
+++ JS/scripting.js
@@ -1,12 +1,20 @@
 /**
  * @author evan lay
  */
-$(window).load(function() 
-{
-	$.getJSON("https://api.github.com/events", function(result){
+
+$(document).ready(function() {
+    $.getJSON("https://api.github.com/events", function(result){
         $.each(result, function(i, field){
-            $("#eventList").append("<p>" + (i+1) + ": " + field.actor.login + "<br>" + field.type + "<br>" + field.repo.url + "<p>");
+            var num = ("0" + (i+1)).slice(-2);
+            $("#eventList").append("<li id = 'li" + num + "'><p>" + num + ": " + field.actor.login + "<br>" + field.type + "<br>" +
+            "<a href= '" + field.repo.url + "'>" + field.repo.url + "</a></p></li><br>");
+            $('#li'+num).append("<p id = 'p" + num + "' class = 'moreInfo'> Time Created: " + field.created_at +"</p>");
         });
+    });
+    
+     $(document).on('click', 'li', function () {;
+        var num = this.id.substr(2,3);
+        $('#p' + num).toggle();
     });
 });
 

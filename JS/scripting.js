90a809da13e95dcd8c3018af8a20389a7cc4c855
Evan Lay <evanrlay@gmail.com>
2016-02-22 14:16:36 +0000

    Made touch friendly on mobile devices

--- JS/scripting.js
+++ JS/scripting.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
     $.getJSON("https://api.github.com/events", function(result){
         $.each(result, function(i, field){
             var num = ("0" + (i+1)).slice(-2);
+            var arr = field.repo.url.split("/");
+            console.log(arr.toString());
             $("#eventList").append("<li id = 'li" + num + "'><p>" + num + ": " + field.actor.login + "<br>" + field.type + "<br>" +
-            "<a href= '" + field.repo.url + "'>" + field.repo.url + "</a></p></li><br>");
-            $('#li'+num).append("<p id = 'p" + num + "' class = 'moreInfo'> Time Created: " + field.created_at +"</p>");
+            "Repo: " + "<a href= '" + field.repo.url + "'>" + arr[4] + "</a></p></li><br>");
+            $('#li'+num).append("<p div data-role='collapsible' id = 'p" + num + "' class = 'moreInfo'> Time Created: " + field.created_at +"</p>");
         });
     });
     
@@ -16,5 +18,12 @@ $(document).ready(function() {
         var num = this.id.substr(2,3);
         $('#p' + num).toggle();
     });
+    
+     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+     $(document).on('touchend', 'li', function () {;
+        var num = this.id.substr(2,3);
+        $('#p' + num).toggle();
+    });
+     }
 });
 

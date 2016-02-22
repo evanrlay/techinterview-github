54f84c1ce9d17fae091e9e8b70d75dd473acf1f7
Evan Lay <evanrlay@gmail.com>
2016-02-22 21:09:34 +0000

    Optimizing mobile interactions

--- JS/scripting.js
+++ JS/scripting.js
@@ -7,22 +7,21 @@ $(document).ready(function() {
         $.each(result, function(i, field){
             var num = ("0" + (i+1)).slice(-2);
             var arr = field.repo.url.split("/");
-            console.log(arr.toString());
             $("#eventList").append("<li id = 'li" + num + "'><p>" + num + ": " + field.actor.login + "<br>" + field.type + "<br>" +
             "Repo: " + "<a href= '" + field.repo.url + "'>" + arr[4] + "</a></p></li><br>");
-            $('#li'+num).append("<p div data-role='collapsible' id = 'p" + num + "' class = 'moreInfo'> Time Created: " + field.created_at +"</p>");
+            $('#eventList').append("<p id = 'p" + num + "' class = 'moreInfo'> Time Created: " + field.created_at +"</p>");
         });
     });
     
-     $(document).on('click', 'li', function () {;
+     $(document).on('click', 'li', function () {
         var num = this.id.substr(2,3);
         $('#p' + num).toggle();
     });
     
      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-     $(document).on('touchend', 'li', function () {;
+     $(document).on('touchend', 'li', function () {
         var num = this.id.substr(2,3);
-        $('#p' + num).toggle();
+        $('#p' + num).slideToggle(500);
     });
      }
 });

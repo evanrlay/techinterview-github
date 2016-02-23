57fefa36979e6fddb1b31eb8f4ff71cc79510954
Evan Lay <evanrlay@gmail.com>
2016-02-23 15:39:38 +0000

    Added avatars and styled them

--- JS/scripting.js
+++ JS/scripting.js
@@ -7,36 +7,36 @@ $(document).ready(function() {
         $.each(result, function(i, field){
             var num = ("0" + (i+1)).slice(-2);
             var arr = field.repo.url.split("/");
-            $("#eventList").append("<li id = 'li" + num + "'><p>" + num + ": " + field.actor.login + "<br>" + field.type + "<br>" +
-            "Repo: " + "<a href= '" + field.repo.url + "'>" + arr[4] + "</a></p></li><br>");
+            $("#eventList").append("<li id = 'li" + num + "'><img src = '" + field.actor.avatar_url + "' alt = User Avatar'><p>" + num + ": " + 
+            field.actor.login + "<br>" + field.type + "<br>" + "Repo: " + "<a href= '" + field.repo.url + "'>" + arr[4] + "</a></p></li><br>");
             $('#eventList').append("<p id = 'p" + num + "' class = 'moreInfo'> Time Created: " + field.created_at +"</p>");
         });
     });
+    
+});
 
-    var dragging = false;
-    
-    $("body").on("touchmove", function(){
-     dragging = true;
-    });
-    
-    $("body").on("touchstart", function(){
-    dragging = false;
-    });
-    
-     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+var dragging = false;
+
+$("body").on("touchmove", function(){
+ dragging = true;
+});
+
+$("body").on("touchstart", function(){
+dragging = false;
+});
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
      $(document).on('touchend', 'li', function () {
         if (dragging)
          return;
         var num = this.id.substr(2,3);
-        $('#p' + num).slideToggle(500);
+        $('#p' + num).slideToggle(400);
     });
-    }
-    else
-    {
-        $(document).on('click', 'li', function () {
-        var num = this.id.substr(2,3);
-        $('#p' + num).slideToggle(500);
+}
+else
+{
+    $(document).on('click', 'li', function () {
+    var num = this.id.substr(2,3);
+    $('#p' + num).slideToggle(500);
     });
-    }
-});
-
+}
